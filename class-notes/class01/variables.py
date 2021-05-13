1768589ee2337efaabbc7994627dcedc6a0f6857
George <georgeoustapas@gmail.com>
2021-05-13 20:13:27 +0100

    first commit

---
+++ class-notes/class01/variables.py
@@ -0,0 +1,37 @@
+print("hello")
+
+
+# Declare two variables `num1` and `num2` and assign them to any numbers you'd like.
+num1 = 10
+num2 = 20
+# Set `num1` to the result of subtracting `num1` from the `num2`.
+num1 = num2 - num1
+
+num3 = num2 % 2
+num1 += 2
+
+print(num1)
+# Create a new variable `num3` that will help us tell if `num2` is even or odd.
+
+# Using shorthand, add 5 to `num1`.
+
+# Print out `num1`, `num2`, and `num3`
+
+
+# STRINGS
+
+name = 'Marty'
+
+car = 'Delorean'
+
+speed = "88"
+
+print(name)
+
+print(car)
+
+print(speed)
+
+print(int(speed) +  4)
+
+print(type(int(speed)))
---
+++ variables/example.py
@@ -0,0 +1,16 @@
+# Make a variable to hold the number of guitars you own (3).
+
+my_guitars = 3
+
+
+# Make a variable to hold the number of guitars Nikhil owns (1).
+
+Nikhil_guitars = 1
+
+
+# You give 2 of your guitars to Nikhil, so subtract 2 from you and add 2 to Nikhil.
+
+
+print(my_guitars -2)
+
+print(Nikhil_guitars +2)
